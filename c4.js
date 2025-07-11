dffbc432ed431978fedadd1f86e5289d31470f83
Tom Corley <tomcorley86@gmail.com>
2025-07-11 13:56:41 +0100

    Computer vs Computer game working.

--- c4.js
+++ c4.js
@@ -1,11 +1,12 @@
 class Game {
-    constructor(p1, p2, rows=6, cols=7) {
+    constructor(p1, p2, rows=6, cols=7, win_req=4) {
         // Assigning players and colours
         this.p1 = p1
         this.p1.setColour("R")
         this.p2 = p2
         this.p2.setColour("Y")
         this.players = [p1, p2]
+        this.win_req = win_req
 
         // Creating 6 (rows) x 7 (columns) grid and set to keep track of full squares
         this.rows = rows
@@ -14,17 +15,21 @@ class Game {
         for (let i = 0; i < this.rows; i++) {
             let r = [];
             for (let j = 0; j < this.cols; j++) {
-                r.push(0)
+                r.push(" ")
             }
             this.grid.push(r)
         }
 
-        console.log(this.grid);
+        this.col_capacity = [];
+        for (let i = 0; i < this.cols; i++) {
+            this.col_capacity.push(this.rows)
+        }
         this.free_squares = this.grid.length * this.grid[0].length;
-        this.full = new Set()
 
         // 0 for ongoing, 1 for p1 win, 2 for p2 win, 3 for draw (no more squares)
         this.state = 0 
+        this.printGrid()
+        this.playGame()
     }
 
     playGame() {
@@ -35,7 +40,7 @@ class Game {
 
             // Player move
             this.playerTurn(this.players[turn])
-            //this.printGrid()
+            this.printGrid()
 
             // Swap whose turn it is
             turn = 1 - turn;
@@ -59,23 +64,19 @@ class Game {
     computerTurn(player) {
         // JS doesnt have tuples so stringigy
         while (true) {
-            let row = Math.floor(6 * Math.random());
-            let col = Math.floor(7 * Math.random());
-            let coord = row+","+col
-
-            if (!this.full.has(coord)) {
-                console.log(`Accepted co-ordinates: ${row},${col}`);
-                this.placeCounter(player, coord)
+            let col = Math.floor(this.cols * Math.random());
+            if (this.col_capacity[col]) {
+                console.log(`Accepted column: ${col}`);
+                this.placeCounter(player, col)
                 break;
             }
         }
     }
 
-    placeCounter(player, coord) {
-        this.full.add(coord)
-        this.grid[coord[0]][coord[2]] = player.colour   
+    placeCounter(player, col) {
+        this.grid[this.col_capacity[col]-1][col] = player.colour   
+        this.col_capacity[col] -= 1
         this.free_squares -= 1;
-        console.log(this.free_squares);
         if (this.free_squares == 0) {
             this.state = 3;
         }
@@ -83,6 +84,8 @@ class Game {
     // Warwick Wellbeing team:  02476 575570
 
     finaliseGame() {
+        this.printGrid()
+        console.log(`Spaces left: ${this.free_squares}`);
         let end_str = "Game Finished: "
         if (this.state == 0) {
             throw "Game ended prematurely";
@@ -97,15 +100,115 @@ class Game {
     }
 
     printGrid() {
-        console.log(this.grid);; // Nice formatted printing of grid
+        let str_grid = ""
+        let edge = "-".repeat(4*this.cols+1)+"\n"
+        str_grid += edge
+        for (let i = 0; i < this.rows; i++) {
+            let str_row = "| "
+            for (let j = 0; j < this.cols; j++) {
+                str_row += this.grid[i][j] 
+                str_row += " | "
+            }
+            str_grid += str_row
+            str_grid += "\n"
+            str_grid += edge 
+        }
+        console.log(str_grid);; // Nice formatted printing of grid
     }
 
-    isWinner() { 
+    char
+
+    isWinner() {
+        return (this.rowWinner() || this.colWinner() || this.diagWinner())
+    }
+
+    rowWinner() { 
         // Check each row, check each diagonal, check each column
         //  return true and update state to winner if connected 4 is found
+        // Two pointers approach to rows, loop for each row
+        for (let i = 0; i < this.rows; i++) {
+            let l = 0;
+            let colour;
+            while (l < this.cols) {
+                // If space is empty, skip to next
+                if (this.grid[i][l] === " ") {
+                    l += 1
+                    continue;
+                }
+
+                // Otherwise proceed while direction is same, using the second pointer
+                colour = this.grid[i][l]
+                let strip_len = 1
+                let r = l + 1
+                while (r < this.grid[0].length && this.grid[i][r] === colour) {
+                    strip_len += 1
+                    console.log(strip_len);
+                    r += 1
+                }
+
+                // Report win if necessary
+                if (strip_len == 4) {
+                    console.log(`Winning Row: ${i}`);
+                    if (colour == "R") {
+                        this.state = 1
+                    }
+                    else {
+                        this.state = 2
+                    }
+                    return true;
+                }
+
+                // Otherwise set left pointer to right pointer
+                l = r
+            }
+        }
+        return false;
     }
 
+    colWinner() {
+        for (let i = 0; i < this.cols; i++) {
+            //console.log(`Column ${i}`);
+            let l = 0;
+            let colour;
+            while (l < this.rows) {
+                // If space is empty, skip to next
+                if (this.grid[l][i] === " ") {
+                    l += 1
+                    continue;
+                }
 
+                // Otherwise proceed while direction is same, using the second pointer
+                //console.log(`Col: ${i}, checking for strip.`);
+                colour = this.grid[l][i]
+                let strip_len = 1
+                let r = l + 1
+                while (r < this.rows && this.grid[r][i] === colour) {
+                    strip_len += 1
+                    r += 1
+                }
+
+                // Report win if necessary
+                if (strip_len == 4) {
+                    console.log(`Winning Column: ${i}`);
+                    if (colour == "R") {
+                        this.state = 1
+                    }
+                    else {
+                        this.state = 2
+                    }
+                    return true;
+                }
+
+                // Otherwise set left pointer to right pointer
+                l = r
+            }
+        }
+        return false;
+    }
+
+    diagWinner() {
+        return false;
+    }
 }
 
 class Player {
@@ -140,5 +243,5 @@ class ComputerPlayer extends Player {
 const red = new ComputerPlayer()
 const yellow = new ComputerPlayer()
 const game = new Game(red, yellow)
-game.playGame()
+//game.playGame()
 // */
